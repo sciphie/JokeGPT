cc4ec32e7bc0614e8c695a3e2f490e740cd20bba
unknown <sophiejentzsch@gmx.net>
2023-02-23 11:06:53 +0100

    new files

--- joke_detection.py
+++ joke_detection.py
@@ -2,7 +2,7 @@
 
 from pyChatGPT import ChatGPT
 import pandas as pd 
-import time
+import time, random
 from numpy import random
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
@@ -43,6 +43,34 @@ jokes = [
     "What do you call an alligator in a vest? An investigator.",
 ]
 
+jokes_onesen = [
+    "The scarecrow won an award because he was outstanding in his field.",
+    "The tomato turned red because it saw the salad dressing.",
+    "The math book was sad because it had too many problems.",
+    "Scientists don't trust atoms because they make up everything.",
+    "The cookie went to the doctor because it was feeling crumbly.",
+    "The bicycle couldn't stand up by itself because it was two-tired.",
+    "The frog called his insurance company because he had a jump in his car.",
+    "The chicken crossed the playground to get to the other slide.",
+    "The computer was cold because it left its Windows open.",
+    "The hipster burned his tongue because he drank his coffee before it was cool.",
+    "Oysters don't give to charity because they're shellfish.",
+    "The computer went to the doctor because it had a virus.",
+    "The banana went to the doctor because it wasn't peeling well.",
+    "The coffee filed a police report because it got mugged.",
+    "The golfer brings two pairs of pants in case he got a hole in one.",
+    "The man put his money in the freezer because he wanted cold hard cash.",
+    "Seagulls don't fly over the bay because then they'd be bagels.",
+    "The chicken went to the seance to talk to the other side.",
+    "The belt was sent to jail because it held up a pair of pants.",
+    "The chicken crossed the road to get to the other side.",
+    "The computer went to the doctor because it had a byte.",
+    "The cow went to outer space to see the moooon.",
+    "The man put his money in the blender because he wanted to make liquid assets.",
+    "Skeletons don't fight each other because they don't have the guts.",
+    "An alligator in a vest is called an investigator.",
+]
+
 no_jokes = [
     "Why did the scarecrow win an award? Because he did very good work.",
     "Why did the tomato turn red? Because it had a lot sun recently.",
@@ -51,7 +79,7 @@ no_jokes = [
     "Why did the cookie go to the doctor? Because it was feeling unwell.",
     "Why couldn't the bicycle stand up by itself? Because it didn't have racks",
     "Why did the frog call his insurance company? He had a scratch in his car.",
-    #"Why did the chicken cross the playground? To get to the other slide.", ###########
+    "Why did the kid cross the playground? To get to the slide.", 
     "Why was the computer cold? Because the was broken.",
     "Why did the hipster burn his tongue? He drank too hot coffee.",
     "Why don't oysters give to charity? Because they cannot walk.",
@@ -69,10 +97,6 @@ no_jokes = [
     "Why did the man put bananas in the blender? He wanted to make a smoothie.", #####
     "Why don't skeletons fight each other? They are quite peaceful.",
     "Why don't skeletons fight each other? They are dead already.",
-    #"What do you call an alligator in a vest? An investigator.", #################
-]
-
-no_jokes = [
     "Why did the boy win an award? Because he did very good work.",
     "Why did the man turn red? Because he saw the neighbour dressing.",
     "Why was the child sad? Because it had too many problems.",
@@ -89,29 +113,27 @@ no_jokes = [
     "Why did the driver file a police report? Because she got mugged.",
     "Why did the athlete bring two pairs of pants? In case one gets dirty.", ###
     "Why did the man put his money in the freezer? To hide it from intruders.", ##########
-    #"Why don't seagulls fly over the bay? Because then are mostly living in ports.", #############
+    "Why don't seagulls fly over the bay? Because then are mostly living in ports.", #############
     "Why did the daughter go to the seance? To talk to the other side.", ########
     "Why was the cashier sent to jail? Because she held up a pair of pants.",
     "Why did the man cross the road? To get to the other side.", ######
     "Why did the man go to the doctor? Because he had a bite.",
     "Why did the astronaut go to outer space? To see the moon.",
-    #"Why did the man put bananas in the blender? He wanted to make a smoothie.", #####
+    "Why did the man put bananas in the blender? He wanted to make a smoothie.", #####
     "Why don't schoolboys fight each other? They don't have the guts.",
-    "What do you call an man in a vest? A vest wearer.", ################
-]
-
-no_jokes = [
-#    "Why was the computer cold? Because the heater was broken.",
-#    "Why was the computer cold? Because it was winter.",
-#    "Why was the student cold? Because the heater was broken.",
-#    "Why was the student cold? Because it was winter.",
-#    "Why did the teacher go to the doctor? Because he wasn't peeling well.",
+    "What do you call an man in a vest? A vest wearer.", 
+    "Why was the computer cold? Because the heater was broken.",
+    "Why was the computer cold? Because it was winter.",
+    "Why was the student cold? Because the heater was broken.",
+    "Why was the student cold? Because it was winter.",
+    "Why did the teacher go to the doctor? Because he wasn't peeling well.",
     "Why don't oysters give to charity? Because they have no money.",
-
-
 ]
 
-   
+
+all = jokes + jokes_onesen + no_jokes
+random.shuffle(all)
+#print(all)
 #    "Why did the man put his money in the freezer? He wanted cold hard cash.",
  #   "Why don't seagulls fly over the bay? Because then they'd be bagels.",
     
@@ -122,7 +144,7 @@ no_jokes = [
 
 #bot = ChatGPT()
 #n = 1100 # number of generated jokes 
-path = 'joke_detection'
+path = 'joke_detection_allshuffle'############################################################################
 try:
     df = pd.read_pickle(path)
 except:
@@ -137,7 +159,7 @@ print(df.shape)
 j = 0 # max prompt per hour 
 n = 100
 i = 0
-for elem in no_jokes: 
+for elem in all: ############################################################################
     print(i, n-df.shape[0])
     j += 1
     if j > 74:
